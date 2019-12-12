5a904585f17fb48d35c0b75f1ec8aa5c4bdd059e
ayush <58028488+desibabua@users.noreply.github.com>
2019-12-12 22:40:26 +0530

    change display formate of toString

--- src/line.js
+++ src/line.js
@@ -1,17 +1,20 @@
 class Line {
   constructor(a, b, c, d) {
-    this.A = [];
-    this.A.push(a);
-    this.A.push(b);
-    this.B = [];
-    this.B.push(c);
-    this.B.push(d);
+    [this.endA, this.endB] = [
+      [a, b],
+      [c, d]
+    ];
   }
-  get toString(){
-    return `{A : [${this.A[0]},${this.A[1]}] ,B : [${this.B[0]},${this.B[1]}] }`
+  get toString() {
+    return `Line :- (${this.endA[0]},${this.endA[1]}),(${this.endB[0]},${this.endB[1]})`;
   }
-  isEqualto(anotherLine){
-    return this.A[0]==anotherLine.A[0] && this.A[1]==anotherLine.A[1] &&this.B[0]==anotherLine.B[0] &&this.B[1]==anotherLine.B[1]
+  isEqualTo(anotherLine) {
+    return (
+      this.endA[0] == anotherLine.endA[0] &&
+      this.endA[1] == anotherLine.endA[1] &&
+      this.endB[0] == anotherLine.endB[0] &&
+      this.endB[1] == anotherLine.endB[1]
+    );
   }
 }
 
--- test/testForLine.js
+++ test/testForLine.js
@@ -1,30 +1,32 @@
 const assert = require("chai").assert;
-const Line = require("../src/line").Line;
+const { Line } = require("../src/line");
 console.log(Line);
+describe("test for line class", function() {
+  describe("to String", function() {
+    it("toString property should behave in same ways when called by class which is called by same args", function() {
+      const actualValue = new Line(1, 2, 3, 4);
+      const expectedValue = new Line(1, 2, 3, 4);
+      assert.deepStrictEqual(actualValue.toString, expectedValue.toString);
+    });
 
-describe("test line class", function() {
-
-  it("should validate to string property when new line is called with different arguments", function() {
-    const actualValue = new Line(1, 2, 3, 4);
-    const expectedValue = new Line(1, 2, 3, 4);
-    assert.deepStrictEqual(actualValue.toString, expectedValue.toString);
+    it("toString property should not behave in same ways when called by class which is called by different args", function() {
+      const actualValue = new Line(1, 2, 3, 4);
+      const expectedValue = new Line(1, 2, 3, 5);
+      assert.notEqual(actualValue.toString, expectedValue.toString);
+    });
   });
 
-  it("should not validate to string property when new line is called with different arguments", function() {
-    const actualValue = new Line(1, 2, 3, 4);
-    const expectedValue = new Line(1, 2, 3, 5);
-    assert.notEqual(actualValue.toString, expectedValue.toString);
+  describe("isEqualTo", function() {
+    it("should check with another line object ", () => {
+      const actualValue = new Line(1, 2, 3, 4);
+      const anotherLine = new Line(1, 2, 3, 4);
+      assert.equal(actualValue.isEqualTo(anotherLine), true);
+    });
+
+    it("should check with another line object ", () => {
+      const actualValue = new Line(1, 2, 3, 4);
+      const anotherLine = new Line(1, 2, 3, 5);
+      assert.equal(actualValue.isEqualTo(anotherLine), false);
+    });
   });
-
-  it("should check with another line object ",()=>{
-    const actualValue = new Line(1,2,3,4)
-    const anotherLine = new Line(1,2,3,4)
-    assert.equal(actualValue.isEqualto(anotherLine),true)
-  })
-
-  it("should check with another line object ",()=>{
-    const actualValue = new Line(1,2,3,4)
-    const anotherLine = new Line(1,2,3,5)
-    assert.equal(actualValue.isEqualto(anotherLine),false)
-  })
 });
