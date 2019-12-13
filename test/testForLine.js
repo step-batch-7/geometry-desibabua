4319d5528cdd6eae4b9150edb9760d2f3c46a63b
ayush <58028488+desibabua@users.noreply.github.com>
2019-12-13 17:20:19 +0530

    added tests for negative and positive nos.

--- test/testForLine.js
+++ test/testForLine.js
@@ -42,10 +42,28 @@ describe("Line", function() {
   });
 
   describe('length',function() {
-    it('should give length of a given line', function() {
+    it('should give length of a given line with positive values', function() {
       let line = new Line({ x: 0, y: 0 }, { x: 3, y: 4 });
       line = line.length
-      assert.strictEqual(line, 5);
+      assert.equal(line, 5);
+
+      line = new Line({ x: 0, y: 0 }, { x: 0, y: 4 });
+      line = line.length
+      assert.equal(line, 4);
+
+      line = new Line({ x: 2, y: 0 }, { x: 0, y: 0});
+      line = line.length
+      assert.notEqual(line, 4);
+    });
+
+    it('should give length of a given line with negative values', function() {
+      let line = new Line({ x: 0, y: 0 }, { x: -3, y: 4 });
+      line = line.length
+      assert.equal(line, 5);
+
+      line = new Line({ x: 0, y: 0 }, { x: -3, y: -4 });
+      line = line.length
+      assert.equal(line, 5);
     });
   });
 });
