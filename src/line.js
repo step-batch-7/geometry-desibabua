b5459ed82f5dafb44b7a884cb41a100012f5fb6e
ayush <58028488+desibabua@users.noreply.github.com>
2019-12-13 13:02:03 +0530

    refactoring isEqual method and done assignment by destructuring

--- src/line.js
+++ src/line.js
@@ -1,13 +1,9 @@
 const isPointsEqual = (point1, point2) =>
   point1.x === point2.x && point1.y === point2.y;
 
-const isTypeSimilar = (line1, line2) =>
-  line1 instanceof Line && line2 instanceof Line;
-
 class Line {
   constructor(endA, endB) {
-    this.endA = { ...endA };
-    this.endB = { ...endB };
+    [this.endA,this.endB] = [{ ...endA },{ ...endB }];
   }
 
   get toString() {
@@ -16,7 +12,7 @@ class Line {
 
   isEqualTo(anotherLine) {
     return (
-      isTypeSimilar(this, anotherLine) &&
+      anotherLine instanceof Line &&
       isPointsEqual(this.endA, anotherLine.endA) &&
       isPointsEqual(this.endB, anotherLine.endB)
     );
