38a4657fbf66db8d3ec2b5c4cf65a8191fbd0b3c
ayush <58028488+desibabua@users.noreply.github.com>
2019-12-13 16:14:49 +0530

    modified Assignment in constructor

--- src/line.js
+++ src/line.js
@@ -1,9 +1,10 @@
-const arePointsEqual = (point1, point2) =>
-  point1.x === point2.x && point1.y === point2.y;
+const arePointsEqual = (pointA, pointB) =>
+  pointA.x === pointB.x && pointA.y === pointB.y;
 
 class Line {
   constructor(endA, endB) {
-    [this.endA, this.endB] = [{ ...endA }, { ...endB }];
+    this.endA = { x: endA.x, y: endA.y };
+    this.endB = { x: endB.x, y: endB.y };
   }
 
   get toString() {
@@ -13,13 +14,11 @@ class Line {
   }
 
   isEqualTo(anotherLine) {
-    if (anotherLine instanceof Line) {
-      return (
-        arePointsEqual(this.endA, anotherLine.endA) &&
-        arePointsEqual(this.endB, anotherLine.endB)
-      );
-    }
-    return false;
+    if (!(anotherLine instanceof Line)) return false;
+    return (
+      arePointsEqual(this.endA, anotherLine.endA) &&
+      arePointsEqual(this.endB, anotherLine.endB)
+    );
   }
 }
 
