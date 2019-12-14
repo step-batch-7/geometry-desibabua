fdadeef0995656a19373514dbd1240bfe72013d5
ayush <58028488+desibabua@users.noreply.github.com>
2019-12-14 12:16:37 +0530

    added visit function in point class and test for it

--- test/testPoint.js
+++ test/testPoint.js
@@ -1,10 +1,20 @@
-const assert = require('chai').assert
+const assert = require("chai").assert;
 const { Point } = require("../src/point");
 
+describe("Point", function() {
+  describe("toString", function() {
+    it("should give new point", function() {
+      const point = new Point(2, 3);
+      assert.strictEqual(point.toString(), "[Point @(2,3)]");
+    });
+  });
 
-describe("should give new point", function() {
-  it("give_description", function() {
-    const point = new Point(2, 3);
-    assert.strictEqual(point.toString(), "[Point @(2,3)]");
+  describe("value", function() {
+    it("should give value of a point with given function reference", function() {
+      const point = new Point(2, 3);
+      assert.strictEqual(point.visit((x,y)=>x+y),5);
+      assert.strictEqual(point.visit((x,y)=>x*y),6);
+      assert.approximately(point.visit((x,y)=>x/y),0.66,0.1);
+    });
   });
 });
